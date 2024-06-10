21b439e7e5f2e7e9fd176fc93c7f92aece3dff9a
Ryan Fleury <ryan.j.fleury@gmail.com>
2024-06-10 11:12:21 -0700

    merged line table sort experiments

--- src/scratch/ryan_scratch.c
+++ src/scratch/ryan_scratch.c
@@ -4,10 +4,6 @@
 ////////////////////////////////
 //~ rjf: Build Options
 
-#define BUILD_VERSION_MAJOR 0
-#define BUILD_VERSION_MINOR 9
-#define BUILD_VERSION_PATCH 10
-#define BUILD_RELEASE_PHASE_STRING_LITERAL "ALPHA"
 #define BUILD_TITLE "ryan_scratch"
 #define BUILD_CONSOLE_INTERFACE 1
 
@@ -16,65 +12,33 @@
 
 //- rjf: [lib]
 #include "lib_rdi_format/rdi_format.h"
-#include "lib_rdi_format/rdi_format_parse.h"
 #include "lib_rdi_format/rdi_format.c"
-#include "lib_rdi_format/rdi_format_parse.c"
+#include "third_party/rad_lzb_simple/rad_lzb_simple.h"
+#include "third_party/rad_lzb_simple/rad_lzb_simple.c"
 
 //- rjf: [h]
 #include "base/base_inc.h"
 #include "os/os_inc.h"
 #include "task_system/task_system.h"
-#include "rdi_make_local/rdi_make_local.h"
-#include "mdesk/mdesk.h"
-#include "hash_store/hash_store.h"
-#include "file_stream/file_stream.h"
-#include "text_cache/text_cache.h"
-#include "path/path.h"
-#include "txti/txti.h"
+#include "rdi_make/rdi_make_local.h"
 #include "coff/coff.h"
-#include "pe/pe.h"
 #include "codeview/codeview.h"
 #include "codeview/codeview_stringize.h"
 #include "msf/msf.h"
 #include "pdb/pdb.h"
 #include "pdb/pdb_stringize.h"
-#include "rdi_from_pdb/rdi_from_pdb.h"
-#include "regs/regs.h"
-#include "regs/rdi/regs_rdi.h"
-#include "type_graph/type_graph.h"
-#include "dbgi/dbgi.h"
-#include "demon/demon_inc.h"
-#include "eval/eval_inc.h"
-#include "unwind/unwind.h"
-#include "ctrl/ctrl_inc.h"
 
 //- rjf: [c]
 #include "base/base_inc.c"
 #include "os/os_inc.c"
 #include "task_system/task_system.c"
-#include "rdi_make_local/rdi_make_local.c"
-#include "mdesk/mdesk.c"
-#include "hash_store/hash_store.c"
-#include "file_stream/file_stream.c"
-#include "text_cache/text_cache.c"
-#include "path/path.c"
-#include "txti/txti.c"
+#include "rdi_make/rdi_make_local.c"
 #include "coff/coff.c"
-#include "pe/pe.c"
 #include "codeview/codeview.c"
 #include "codeview/codeview_stringize.c"
 #include "msf/msf.c"
 #include "pdb/pdb.c"
 #include "pdb/pdb_stringize.c"
-#include "rdi_from_pdb/rdi_from_pdb.c"
-#include "regs/regs.c"
-#include "regs/rdi/regs_rdi.c"
-#include "type_graph/type_graph.c"
-#include "dbgi/dbgi.c"
-#include "demon/demon_inc.c"
-#include "eval/eval_inc.c"
-#include "unwind/unwind.c"
-#include "ctrl/ctrl_inc.c"
 
 ////////////////////////////////
 //~ rjf: Entry Point
@@ -82,5 +46,13 @@
 internal void
 entry_point(CmdLine *cmdline)
 {
-  
+  Arena *arena = arena_alloc();
+  RDIM_SortKey keys_unsorted[] = {{1, (void *)2}, {2}, {3}, {1, (void *)1}, {2}, {3}, {1, (void *)3}, {2}, {3}, {1, (void *)4}, {2}, {3}, {1, (void *)5}, {2}, {3}};
+  U64 keys_count = ArrayCount(keys_unsorted);
+  RDIM_SortKey *keys_sorted = rdim_sort_key_array(arena, keys_unsorted, keys_count);
+  for(U64 idx = 0; idx < keys_count; idx += 1)
+  {
+    printf("%I64u (%I64u),", keys_sorted[idx].key, (U64)keys_sorted[idx].val);
+  }
+  printf("\n");
 }
