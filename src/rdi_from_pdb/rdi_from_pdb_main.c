327fec315527c6ebd1bd369b16d04cff86b350bf
Ryan Fleury <ryan.j.fleury@gmail.com>
2024-10-29 16:50:37 -0700

    fix rdi_from_pdb build

--- src/rdi_from_pdb/rdi_from_pdb_main.c
+++ src/rdi_from_pdb/rdi_from_pdb_main.c
@@ -23,8 +23,8 @@
 //- rjf: [h]
 #include "base/base_inc.h"
 #include "os/os_inc.h"
+#include "async/async.h"
 #include "task_system/task_system.h"
-#include "task_system/task_system2.h"
 #include "rdi_make/rdi_make_local.h"
 #include "coff/coff.h"
 #include "codeview/codeview.h"
@@ -39,8 +39,8 @@
 //- rjf: [c]
 #include "base/base_inc.c"
 #include "os/os_inc.c"
+#include "async/async.c"
 #include "task_system/task_system.c"
-#include "task_system/task_system2.c"
 #include "rdi_make/rdi_make_local.c"
 #include "coff/coff.c"
 #include "codeview/codeview.c"
