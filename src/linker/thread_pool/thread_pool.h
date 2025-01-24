8196ef0a6e155ed1e32395b85ef9cbcc1e0cc146
Nikita Smith <git.nikita.smith@gmail.com>
2025-01-24 10:33:42 -0800

    tweaks and fixed in thread pool
    - created a separate entry point for shared workers
    - fixed race condition in tp_run_tasks where worker would read
      modified task count and cause unpredictable behavior

--- src/linker/thread_pool/thread_pool.h
+++ src/linker/thread_pool/thread_pool.h
@@ -27,17 +27,20 @@ typedef struct TP_Worker
 
 typedef struct TP_Context
 {
+  B32          is_live;
+  OS_Handle    exec_semaphore;
   OS_Handle    task_semaphore;
   OS_Handle    main_semaphore;
-  B32          is_live;
+
   U32          worker_count;
   TP_Worker   *worker_arr;
-  TP_Arena    *worker_arena;
-  U64          task_count;
+
+  TP_Arena    *task_arena;
   TP_TaskFunc *task_func;
   void        *task_data;
-  volatile S64 task_left_count;
-  volatile U64 task_done_count;
+  U64          task_count;
+  U64          task_done;
+  S64          task_left;
 } TP_Context;
 
 internal TP_Context * tp_alloc(Arena *arena, U32 worker_count, String8 name);
