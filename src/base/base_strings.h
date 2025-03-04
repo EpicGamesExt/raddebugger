53bff614e6354ad94c5e6565e667391e9fb6c7de
Nikita Smith <git.nikita.smith@gmail.com>
2025-03-03 17:04:41 -0800

    moved LEB helpers to DWARF layer

--- src/base/base_strings.h
+++ src/base/base_strings.h
@@ -407,8 +407,6 @@ internal void * str8_deserial_get_raw_ptr(String8 string, U64 off, U64 size);
 internal U64    str8_deserial_read_cstr(String8 string, U64 off, String8 *cstr_out);
 internal U64    str8_deserial_read_windows_utf16_string16(String8 string, U64 off, String16 *str_out);
 internal U64    str8_deserial_read_block(String8 string, U64 off, U64 size, String8 *block_out);
-internal U64    str8_deserial_read_uleb128(String8 string, U64 off, U64 *value_out);
-internal U64    str8_deserial_read_sleb128(String8 string, U64 off, S64 *value_out);
 #define str8_deserial_read_array(string, off, ptr, count) str8_deserial_read((string), (off), (ptr), sizeof(*(ptr))*(count), sizeof(*(ptr)))
 #define str8_deserial_read_struct(string, off, ptr)       str8_deserial_read_array(string, off, ptr, 1)
 
