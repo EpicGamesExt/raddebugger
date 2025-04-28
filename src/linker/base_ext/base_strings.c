3a879614509a4430211ee529582ece65ab01d0f0
Nikita Smith <git.nikita.smith@gmail.com>
2025-04-28 13:55:26 -0700

    delete string compars form linker layer

--- src/linker/base_ext/base_strings.c
+++ src/linker/base_ext/base_strings.c
@@ -1,66 +1,6 @@
 // Copyright (c) 2024 Epic Games Tools
 // Licensed under the MIT license (https://opensource.org/license/mit/)
 
-internal int
-str8_compar(String8 a, String8 b, B32 ignore_case)
-{
-  int cmp = 0;
-  U64 size = Min(a.size, b.size);
-  if (ignore_case) {
-    for (U64 i = 0; i < size; ++i) {
-      U8 la = char_to_lower(a.str[i]);
-      U8 lb = char_to_lower(b.str[i]);
-      if (la < lb) {
-        cmp = -1;
-        break;
-      } else if (la > lb) {
-        cmp = +1;
-        break;
-      }
-    } 
-  } else {
-    for (U64 i = 0; i < size; ++i) {
-      if (a.str[i] < b.str[i]) {
-        cmp = -1;
-        break;
-      } else if (a.str[i] > b.str[i]) {
-        cmp = +1;
-        break;
-      }
-    } 
-  }
-  
-  if (cmp == 0) {
-    // shorter prefix must precede longer prefixes
-    if (a.size > b.size) {
-      cmp = +1;
-    } else if (b.size > a.size) {
-      cmp = -1;
-    }
-  }
-  
-  return cmp;
-}
-
-internal int
-str8_compar_ignore_case(const void *a, const void *b)
-{
-  return str8_compar(*(String8*)a, *(String8*)b, 1);
-}
-
-internal int
-str8_compar_case_sensitive(const void *a, const void *b)
-{
-  return str8_compar(*(String8*)a, *(String8*)b, 0);
-}
-
-internal int
-str8_is_before_case_sensitive(const void *a, const void *b)
-{
-  int cmp = str8_compar_case_sensitive(a, b);
-  return cmp < 0;
-}
-
 internal String8Node *
 str8_list_push_raw(Arena *arena, String8List *list, void *data_ptr, U64 data_size)
 {
