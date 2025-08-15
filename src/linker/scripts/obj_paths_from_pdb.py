04d2877b40e038dc653139e381e7473d17e0ffe4
Nikita Smith <git.nikita.smith@gmail.com>
2025-08-15 12:57:33 -0700

    strip leading white space

--- src/linker/scripts/obj_paths_from_pdb.py
+++ src/linker/scripts/obj_paths_from_pdb.py
@@ -5,7 +5,7 @@ import os
 def get_sorted_objs(pdb_path):
     result = subprocess.run(["llvm-pdbutil", "dump", "--modules", pdb_path], stdout=subprocess.PIPE, text=True)
     lines = result.stdout.strip().split('\n')
-    filtered_lines = [line for line in lines if line.startswith("Mod ")]
+    filtered_lines = [line for line in lines if line.lstrip().startswith("Mod ")]
     # sort by the obj_path portion (line format: "Mod <imod> <obj_path>")
     def extract_path(line):  return line.split(maxsplit=2)[2].lower()
     sorted_lines = sorted(filtered_lines, key=extract_path)
@@ -13,4 +13,4 @@ def get_sorted_objs(pdb_path):
 
 if __name__ == "__main__":
     sorted_objs = get_sorted_objs(sys.argv[1])
-    for l in sorted_objs: print(l)
+    for l in sorted_objs: print(l.lstrip())
