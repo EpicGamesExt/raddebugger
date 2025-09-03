43b35d43dc12c87017850113241ad1ab264d7c0b
Nikita Smith <git.nikita.smith@gmail.com>
2025-09-02 21:59:00 -0700

    change lib sort to preserve input order

--- src/linker/lnk_lib.h
+++ src/linker/lnk_lib.h
@@ -41,6 +41,7 @@ typedef struct LNK_LibList
 typedef struct
 {
   struct LNK_Input  **inputs;
+  U64                 lib_id_base;
   U64                 next_free_lib_idx;
   U64                 valid_libs_count;
   U64                 invalid_libs_count;
@@ -54,12 +55,12 @@ typedef struct
 internal int lnk_lib_node_is_before(void *a, void *b);
 internal int lnk_lib_node_ptr_is_before(void *raw_a, void *raw_b);
 
-internal B32              lnk_lib_from_data(Arena *arena, String8 data, String8 path, LNK_Lib *lib_out);
+internal B32              lnk_lib_from_data(Arena *arena, String8 data, String8 path, U64 input_idx, LNK_Lib *lib_out);
 internal LNK_Lib **       lnk_array_from_lib_list(Arena *arena, LNK_LibList list);
 internal void             lnk_lib_list_push_node(LNK_LibList *list, LNK_LibNode *node);
 internal LNK_LibNodeArray lnk_lib_list_push_parallel(TP_Context *tp, TP_Arena *arena, LNK_LibList *list, U64 inputs_count, struct LNK_Input **inputs);
 
-internal B32 lnk_flag_member_as_queued(LNK_Lib *lib, U32 member_idx, LNK_Symbol *trigger);
+internal B32 lnk_lib_set_link_symbol(LNK_Lib *lib, U32 member_idx, LNK_Symbol *trigger);
 
 internal B32 lnk_search_lib(LNK_Lib *lib, String8 symbol_name, U32 *member_idx_out);
 
