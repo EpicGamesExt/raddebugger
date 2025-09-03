a64f8f4196d227af04ed86a53fb2cedef175402d
Nikita Smith <git.nikita.smith@gmail.com>
2025-09-02 22:02:48 -0700

    clean up over base strings in the linker layer

--- src/linker/base_ext/base_strings.c
+++ src/linker/base_ext/base_strings.c
@@ -1,52 +1,10 @@
 // Copyright (c) 2025 Epic Games Tools
 // Licensed under the MIT license (https://opensource.org/license/mit/)
 
-internal String8Node *
-str8_list_push_raw(Arena *arena, String8List *list, void *data_ptr, U64 data_size)
+internal B32
+str8_starts_with(String8 string, String8 expected_prefix)
 {
-  String8 data = str8((U8 *)data_ptr, data_size);
-  String8Node *node = str8_list_push(arena, list, data);
-  return node;
-}
-
-internal U64
-str8_list_push_pad(Arena *arena, String8List *list, U64 offset, U64 align)
-{
-  U64 pad_size = AlignPow2(offset, align) - offset;
-  U8 *pad = push_array(arena, U8, pad_size);
-  MemorySet(pad, 0, pad_size);
-  str8_list_push(arena, list, str8(pad, pad_size));
-  return pad_size;
-}
-
-internal U64
-str8_list_push_pad_front(Arena *arena, String8List *list, U64 offset, U64 align)
-{
-  U64 pad_size = AlignPow2(offset, align) - offset;
-  U8 *pad = push_array(arena, U8, pad_size);
-  MemorySet(pad, 0, pad_size);
-  str8_list_push_front(arena, list, str8(pad, pad_size));
-  return pad_size;
-}
-
-internal String8List
-str8_list_arr_concat(String8List *v, U64 count)
-{
-  String8List result = {0};
-  for (U64 i = 0; i < count; i += 1) {
-    str8_list_concat_in_place(&result, &v[i]);
-  }
-  return result;
-}
-
-internal String8Node *
-str8_list_push_many(Arena *arena, String8List *list, U64 count)
-{
-  String8Node *arr = push_array(arena, String8Node, count);
-  for (U64 i = 0; i < count; ++i) {
-    str8_list_push_node(list, arr + i);
-  }
-  return arr;
+  return str8_match(str8_prefix(string, expected_prefix.size), expected_prefix, 0);
 }
 
 internal String8Node *
@@ -61,12 +19,5 @@ str8_list_pop_front(String8List *list)
     SLLQueuePop(list->first, list->last);
   }
   return node;
-}
-
-internal U64
-hash_from_str8(String8 string)
-{
-  XXH64_hash_t hash64 = XXH3_64bits(string.str, string.size);
-  return hash64;
 }
 
