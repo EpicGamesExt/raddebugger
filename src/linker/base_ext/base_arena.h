30e21a84eaa757b8cab18323b48e6b69713abd48
Nikita Smith <git.nikita.smith@gmail.com>
2026-03-31 17:51:55 -0700

    arena helper for dealing with buffer preallocation for workers

--- src/linker/base_ext/base_arena.h
+++ src/linker/base_ext/base_arena.h
@@ -3,11 +3,9 @@
 
 #pragma once
 
-internal U64 *   push_u64           (Arena *arena, U64 value);
-internal U32 *   push_array_copy_u32(Arena *arena, U32 *v, U64 count);
-internal U64 *   push_array_copy_u64(Arena *arena, U64 *v, U64 count);
-internal U64 **  push_matrix_u64    (Arena *arena, U64 rows, U64 columns);
-
+internal U64 *    push_u64           (Arena *arena, U64 value);
+internal U32 *    push_array_copy_u32(Arena *arena, U32 *v, U64 count);
+internal U64 *    push_array_copy_u64(Arena *arena, U64 *v, U64 count);
 internal Arena ** alloc_fixed_size_arena_array(Arena *arena, U64 count, U64 res, U64 cmt);
-internal void     release_arena_array(Arena **arr);
+#define alloc_arena_array(_arena_, _arena_count_, _counts_, T) alloc_arena_array_(_arena_, _arena_count_, _counts_, sizeof(T))
 
