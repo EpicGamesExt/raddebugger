b389650136de39f0c55498635ff83034859f2c77
Nikita Smith <git.nikita.smith@gmail.com>
2026-04-21 14:16:45 -0700

    wildcard matcher

--- src/base/base_strings.h
+++ src/base/base_strings.h
@@ -197,6 +197,7 @@ internal String8 backslashed_from_str8(Arena *arena, String8 string);
 #define str8_match_lit(a_lit, b, flags)   str8_match(str8_lit(a_lit), (b), (flags))
 #define str8_match_cstr(a_cstr, b, flags) str8_match(str8_cstring(a_cstr), (b), (flags))
 internal B32 str8_match(String8 a, String8 b, StringMatchFlags flags);
+internal B32 str8_match_wildcard(String8 string, String8 pattern, StringMatchFlags flags);
 internal U64 str8_find_needle(String8 string, U64 start_pos, String8 needle, StringMatchFlags flags);
 internal U64 str8_find_needle_reverse(String8 string, U64 start_pos, String8 needle, StringMatchFlags flags);
 internal B32 str8_is_before(String8 a, String8 b);
