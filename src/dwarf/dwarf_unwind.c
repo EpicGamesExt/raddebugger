EpicGamesExt/raddebugger

commit aea4a4ff9d6ed823b432d94b84fe88054f69265b

eliminate os_core, move into base; eliminate regs, move into arch abstraction layer, define x64 regs in x64 layer; move win32 backend for os core (now base) functionality to top-level win32 layer

src/dwarf/dwarf_unwind.c
@@ -36,7 +36,7 @@ dw_cfi_unwind_init(Arena        *arena,
                    void         *decode_ptr_ud)
 {
   Temp scratch = scratch_begin(&arena, 1);
-
+  
   DW_CFI_Unwind *uw = push_array(arena, DW_CFI_Unwind, 1);
   uw->insts     = dw_parse_cfa_inst_list(arena, fde->insts, cie->code_align_factor, cie->data_align_factor, decode_ptr_func, decode_ptr_ud);
   uw->cie       = cie;
@@ -44,7 +44,7 @@ dw_cfi_unwind_init(Arena        *arena,
   uw->pc        = fde->pc_range.min;
   uw->arch      = arch;
   uw->reg_count = dw_reg_count_from_arch(arch);
-
+  
   // setup initial register rules
   DW_CFA_InstList initial_insts = dw_parse_cfa_inst_list(scratch.arena, cie->insts, cie->code_align_factor, cie->data_align_factor, decode_ptr_func, decode_ptr_ud);
   uw->row       = dw_make_cfi_row(arena, uw->reg_count);
@@ -52,12 +52,12 @@ dw_cfi_unwind_init(Arena        *arena,
   if (!dw_cfi_next_row(arena, uw)) {
     AssertAlways(0 && "unable to interpret initial row instructions");
   }
-
+  
   // make first row from initial rules
   uw->initial_row = uw->row;
   uw->row         = dw_copy_cfi_row(arena, uw->initial_row, uw->reg_count);
   uw->curr_inst   = uw->insts.first;
-
+  
   scratch_end(scratch);
   return uw;
 }
@@ -67,7 +67,7 @@ dw_cfi_next_row(Arena *arena, DW_CFI_Unwind *uw)
 {
   B32              is_row_valid = 0;
   DW_CFA_InstNode *row_inst     = uw->curr_inst;
-
+  
   // skip leading nops
   while (uw->curr_inst) {
     if (uw->curr_inst->v.opcode != DW_CFA_Nop) {
@@ -75,10 +75,10 @@ dw_cfi_next_row(Arena *arena, DW_CFI_Unwind *uw)
     }
     uw->curr_inst = uw->curr_inst->next;
   }
-
+  
   while (uw->curr_inst) {
     DW_CFA_Inst *inst = &uw->curr_inst->v;
-
+    
     // validate register operands
     DW_CFA_OperandType *operand_types = dw_operand_types_from_cfa_op(inst->opcode);
     U64                 operand_count = dw_operand_count_from_cfa_opcode(inst->opcode);
@@ -89,113 +89,113 @@ dw_cfi_next_row(Arena *arena, DW_CFI_Unwind *uw)
         }
       }
     }
-
+    
     switch (inst->opcode) {
-    // Row Creation Instructions
-    case DW_CFA_SetLoc: {
-      uw->pc = inst->operands[0].u64;
-    } break;
-    case DW_CFA_AdvanceLoc:
-    case DW_CFA_AdvanceLoc1:
-    case DW_CFA_AdvanceLoc2:
-    case DW_CFA_AdvanceLoc4: {
-      uw->pc += inst->operands[0].u64;
-    } break;
-
-    // CFA Definition Instructions
-    case DW_CFA_DefCfa:
-    case DW_CFA_DefCfaSf: {
-      U64 reg = inst->operands[0].u64;
-      S64 off = inst->operands[1].s64;
-      uw->row->cfa.rule = DW_CFA_Rule_RegOff;
-      uw->row->cfa.reg  = reg;
-      uw->row->cfa.off  = off;
-    } break;
-    case DW_CFA_DefCfaRegister: {
-      // TODO: report error: this operation is valid only if the current CFA rule is defined to register+offset
-      if (uw->row->cfa.rule != DW_CFA_Rule_RegOff) { goto exit; }
-      U64 reg = inst->operands[0].u64;
-      uw->row->cfa.reg = reg;
-    } break;
-    case DW_CFA_DefCfaOffset:
-    case DW_CFA_DefCfaOffsetSf: {
-      // TODO: report error: this operation is valid only if the current CFA rule is defined to register+offset
-      if (uw->row->cfa.rule != DW_CFA_Rule_RegOff) { goto exit; }
-      uw->row->cfa.off = inst->operands[0].s64;
-    } break;
-    case DW_CFA_DefCfaExpr: {
-      uw->row->cfa.rule = DW_CFA_Rule_Expression;
-      uw->row->cfa.expr = inst->operands[0].block;
-    } break;
-
-    // Register Rule Instructions
-    case DW_CFA_Undefined: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_Undefined;
-    } break;
-    case DW_CFA_SameValue: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_SameValue;
-    } break;
-    case DW_CFA_Offset:
-    case DW_CFA_OffsetExt:
-    case DW_CFA_OffsetExtSf: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_Offset;
-      uw->row->regs[reg].n    = inst->operands[1].s64;
-    } break;
-    case DW_CFA_ValOffset:
-    case DW_CFA_ValOffsetSf: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_ValOffset;
-      uw->row->regs[reg].n    = inst->operands[1].s64;
-    } break;
-    case DW_CFA_Register: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_Register;
-      uw->row->regs[reg].n    = inst->operands[1].s64;
-    } break;
-    case DW_CFA_Expr: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_Expression;
-      uw->row->regs[reg].expr = inst->operands[1].block;
-    } break;
-    case DW_CFA_ValExpr: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg].rule = DW_CFI_RegisterRule_ValExpression;
-      uw->row->regs[reg].expr = inst->operands[1].block;
-    } break;
-    case DW_CFA_Restore:
-    case DW_CFA_RestoreExt: {
-      U64 reg = inst->operands[0].u64;
-      uw->row->regs[reg] = uw->initial_row->regs[reg];
-    } break;
+      // Row Creation Instructions
+      case DW_CFA_SetLoc: {
+        uw->pc = inst->operands[0].u64;
+      } break;
+      case DW_CFA_AdvanceLoc:
+      case DW_CFA_AdvanceLoc1:
+      case DW_CFA_AdvanceLoc2:
+      case DW_CFA_AdvanceLoc4: {
+        uw->pc += inst->operands[0].u64;
+      } break;
       
-    // Row State Instructions
-    case DW_CFA_RememberState: {
-      DW_CFI_Row *new_row = dw_copy_cfi_row(arena, uw->row, uw->reg_count);
-      SLLStackPush(uw->row, new_row);
-    } break;
-    case DW_CFA_RestoreState: {
-      if (uw->row == 0) { goto exit; } // TODO: report error: unbalanced number of pushes and pops
-      DW_CFI_Row *free_row = uw->row;
-      SLLStackPop(uw->row);
-      SLLStackPush(uw->free_rows, free_row);
-    } break;
-
-    case DW_CFA_Nop: {} break;
-
-    default: { NotImplemented; } break; // TODO: report error: unknown CFA opcode
+      // CFA Definition Instructions
+      case DW_CFA_DefCfa:
+      case DW_CFA_DefCfaSf: {
+        U64 reg = inst->operands[0].u64;
+        S64 off = inst->operands[1].s64;
+        uw->row->cfa.rule = DW_CFA_Rule_RegOff;
+        uw->row->cfa.reg  = reg;
+        uw->row->cfa.off  = off;
+      } break;
+      case DW_CFA_DefCfaRegister: {
+        // TODO: report error: this operation is valid only if the current CFA rule is defined to register+offset
+        if (uw->row->cfa.rule != DW_CFA_Rule_RegOff) { goto exit; }
+        U64 reg = inst->operands[0].u64;
+        uw->row->cfa.reg = reg;
+      } break;
+      case DW_CFA_DefCfaOffset:
+      case DW_CFA_DefCfaOffsetSf: {
+        // TODO: report error: this operation is valid only if the current CFA rule is defined to register+offset
+        if (uw->row->cfa.rule != DW_CFA_Rule_RegOff) { goto exit; }
+        uw->row->cfa.off = inst->operands[0].s64;
+      } break;
+      case DW_CFA_DefCfaExpr: {
+        uw->row->cfa.rule = DW_CFA_Rule_Expression;
+        uw->row->cfa.expr = inst->operands[0].block;
+      } break;
+      
+      // Register Rule Instructions
+      case DW_CFA_Undefined: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_Undefined;
+      } break;
+      case DW_CFA_SameValue: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_SameValue;
+      } break;
+      case DW_CFA_Offset:
+      case DW_CFA_OffsetExt:
+      case DW_CFA_OffsetExtSf: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_Offset;
+        uw->row->regs[reg].n    = inst->operands[1].s64;
+      } break;
+      case DW_CFA_ValOffset:
+      case DW_CFA_ValOffsetSf: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_ValOffset;
+        uw->row->regs[reg].n    = inst->operands[1].s64;
+      } break;
+      case DW_CFA_Register: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_Register;
+        uw->row->regs[reg].n    = inst->operands[1].s64;
+      } break;
+      case DW_CFA_Expr: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_Expression;
+        uw->row->regs[reg].expr = inst->operands[1].block;
+      } break;
+      case DW_CFA_ValExpr: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg].rule = DW_CFI_RegisterRule_ValExpression;
+        uw->row->regs[reg].expr = inst->operands[1].block;
+      } break;
+      case DW_CFA_Restore:
+      case DW_CFA_RestoreExt: {
+        U64 reg = inst->operands[0].u64;
+        uw->row->regs[reg] = uw->initial_row->regs[reg];
+      } break;
+      
+      // Row State Instructions
+      case DW_CFA_RememberState: {
+        DW_CFI_Row *new_row = dw_copy_cfi_row(arena, uw->row, uw->reg_count);
+        SLLStackPush(uw->row, new_row);
+      } break;
+      case DW_CFA_RestoreState: {
+        if (uw->row == 0) { goto exit; } // TODO: report error: unbalanced number of pushes and pops
+        DW_CFI_Row *free_row = uw->row;
+        SLLStackPop(uw->row);
+        SLLStackPush(uw->free_rows, free_row);
+      } break;
+      
+      case DW_CFA_Nop: {} break;
+      
+      default: { NotImplemented; } break; // TODO: report error: unknown CFA opcode
     }
-
+    
     uw->curr_inst = uw->curr_inst->next;
     if (uw->curr_inst) {
       if (dw_is_new_row_cfa_opcode(uw->curr_inst->v.opcode)) { break; }
     }
   }
-
+  
   is_row_valid = row_inst != 0;
-exit:;
+  exit:;
   return is_row_valid;
 }
 
@@ -203,7 +203,7 @@ internal DW_CFI_Row *
 dw_cfi_row_from_pc(Arena *arena, Arch arch, DW_CIE *cie, DW_FDE *fde, DW_DecodePtr *decode_ptr_func, void *decode_ptr_ctx, U64 pc)
 {
   Temp scratch = scratch_begin(&arena, 1);
-
+  
   B32            is_row_found = 0;
   U64            reg_count    = dw_reg_count_from_arch(arch);
   DW_CFI_Unwind *uw           = dw_cfi_unwind_init(scratch.arena, arch, cie, fde, decode_ptr_func, decode_ptr_ctx);
@@ -217,7 +217,7 @@ dw_cfi_row_from_pc(Arena *arena, Arch arch, DW_CIE *cie, DW_FDE *fde, DW_DecodeP
     dw_memcpy_cfi_row(row, uw->row, uw->reg_count);
     prev_pc = uw->pc;
   }
-
+  
   // handle last row
   if (!is_row_found) {
     if (contains_1u64(fde->pc_range, pc)) {
@@ -225,116 +225,138 @@ dw_cfi_row_from_pc(Arena *arena, Arch arch, DW_CIE *cie, DW_FDE *fde, DW_DecodeP
       is_row_found = 1;
     }
   }
-
+  
   // copy out final row
   DW_CFI_Row *result = 0;
   if (is_row_found) {
     result = dw_copy_cfi_row(arena, row, reg_count);
   }
-
+  
   scratch_end(scratch);
   return result;
 }
 
 internal MachineOpResult
-dw_compute_cfa(Arch arch,
-               DW_CFI_Row *row,
-               MachineOp_MemRead *mem_read_func,  void *mem_read_ud,
-               MachineOp_RegRead *reg_read_func,  void *reg_read_ud,
-               U64 *cfa_out)
+dw_compute_cfa(Arch arch, DW_CFI_Row *row, void *reg_block, MachineOp_MemRead *mem_read_func, void *mem_read_ud, U64 *cfa_out)
 {
   MachineOpResult unwind_status = MachineOpResult_Fail;
-
-  switch (row->cfa.rule) {
-  case DW_CFA_Rule_Null: break;
-  case DW_CFA_Rule_RegOff: {
-    // TODO: report error (invalid register read)
-    U64 cfa_reg_value = 0;
-    U64 reg_size = dw_reg_size_from_code(arch, row->cfa.reg);
-    AssertAlways(reg_size <= sizeof(cfa_reg_value));
-    unwind_status = reg_read_func(row->cfa.reg, &cfa_reg_value, reg_size, reg_read_ud);
-    if (unwind_status != MachineOpResult_Ok) { break; }
-    *cfa_out = cfa_reg_value + row->cfa.off;
-  } break;
-  case DW_CFA_Rule_Expression: {
-    // TODO: evaluate expression
-    NotImplemented;
-  } break;
+  ARCH_Info *arch_info = arch_info_from_arch(arch);
+  switch(row->cfa.rule)
+  {
+    case DW_CFA_Rule_Null:{}break;
+    case DW_CFA_Rule_RegOff:
+    {
+      // TODO: report error (invalid register read)
+      U64 cfa_reg_value = 0;
+      U64 reg_size = dw_reg_size_from_code(arch, row->cfa.reg);
+      AssertAlways(reg_size <= sizeof(cfa_reg_value));
+      ARCH_RegCode reg_code = arch_reg_code_from_dw(arch, row->cfa.reg);
+      Rng1U16 reg_rng = arch_info->reg_code_rng_table[reg_code];
+      if(!arch_reg_block_read_range(arch_info, reg_block, reg_rng, &cfa_reg_value))
+      {
+        unwind_status = MachineOpResult_Fail;
+        break;
+      }
+      *cfa_out = cfa_reg_value + row->cfa.off;
+    }break;
+    case DW_CFA_Rule_Expression:
+    {
+      // TODO: evaluate expression
+      NotImplemented;
+    }break;
   }
-
   return unwind_status;
 }
 
 internal MachineOpResult
-dw_cfi_apply_register_rules(Arch         arch,
-                            U64          cfa,
-                            DW_CFI_Row  *row,
-                            MachineOp_MemRead  *mem_read_func,  void *mem_read_ud,
-                            MachineOp_RegRead  *reg_read_func,  void *reg_read_ud,
-                            MachineOp_RegWrite *reg_write_func, void *reg_write_ud)
+dw_cfi_apply_register_rules(Arch arch, U64 cfa, DW_CFI_Row *row, void *reg_block, MachineOp_MemRead *mem_read_func, void *mem_read_ud)
 {
   Temp scratch = scratch_begin(0,0);
   MachineOpResult unwind_status = MachineOpResult_Ok;
-
+  
+  ARCH_Info *arch_info = arch_info_from_arch(arch);
+  
   U64   max_reg_size = dw_reg_max_size_from_arch(arch);
   void *reg_buffer   = push_array(scratch.arena, U8, max_reg_size);
-
+  
   U64 reg_count = dw_reg_count_from_arch(arch);
-  for EachIndex(reg_idx, reg_count) {
+  for EachIndex(reg_idx, reg_count)
+  {
     DW_CFI_Register *reg = &row->regs[reg_idx];
-    switch (reg->rule) {
-    case DW_CFI_RegisterRule_Undefined: {} break;
-    case DW_CFI_RegisterRule_SameValue: {} break;
-    case DW_CFI_RegisterRule_Offset: {
-      // read register value from memory
-      U64 addr     = cfa + reg->n;
-      U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
-      unwind_status = mem_read_func(addr, reg_buffer, reg_size, mem_read_ud);
-      if (unwind_status != MachineOpResult_Ok) { goto exit; }
-
-      // write register value to the thread context
-      unwind_status = reg_write_func(reg_idx, reg_buffer, reg_size, reg_write_ud);
-      if (unwind_status != MachineOpResult_Ok) { goto exit; }
-    } break;
-    case DW_CFI_RegisterRule_ValOffset: {
-      // compute register value
-      U64 reg_value = cfa + reg->n;
-
-      // write register value to the thread context
-      U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
-      unwind_status = reg_write_func(reg_idx, &reg_value, reg_size, reg_write_ud);
-      if (unwind_status != MachineOpResult_Ok) { goto exit; }
-    } break;
-    case DW_CFI_RegisterRule_Register: {
-      // read register value from another register
-      U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
-      unwind_status = reg_read_func(reg->n, reg_buffer, reg_size, reg_read_ud);
-      if (unwind_status != MachineOpResult_Ok) { goto exit; }
-
-      // write register value to the thread context
-      unwind_status = reg_write_func(reg_idx, reg_buffer, reg_size, reg_write_ud);
-      if (unwind_status != MachineOpResult_Ok) { goto exit; }
-    } break;
-    case DW_CFI_RegisterRule_Expression: {
-      // TODO: evaluate expression
-      NotImplemented;
-    } break;
-    case DW_CFI_RegisterRule_ValExpression: {
-      // TODO: evaluate expression
-      NotImplemented;
-    } break;
-    case DW_CFI_RegisterRule_Architectural: {
-      NotImplemented;
-    } break;
-    default: { InvalidPath; } break;
+    ARCH_RegCode reg_code = arch_reg_code_from_dw(arch, reg_idx);
+    Rng1U16 reg_rng = arch_info->reg_code_rng_table[reg_code];
+    switch(reg->rule)
+    {
+      default:{}break;
+      case DW_CFI_RegisterRule_Undefined: {} break;
+      case DW_CFI_RegisterRule_SameValue: {} break;
+      case DW_CFI_RegisterRule_Offset:
+      {
+        // read register value from memory
+        U64 addr     = cfa + reg->n;
+        U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
+        unwind_status = mem_read_func(addr, reg_buffer, reg_size, mem_read_ud);
+        if (unwind_status != MachineOpResult_Ok) { goto exit; }
+        
+        // write register value to the thread context
+        if(!arch_reg_block_write_range(arch_info, reg_block, reg_rng, reg_buffer))
+        {
+          unwind_status = MachineOpResult_Fail;
+          goto exit;
+        }
+      }break;
+      case DW_CFI_RegisterRule_ValOffset:
+      {
+        // compute register value
+        U64 reg_value = cfa + reg->n;
+        
+        // write register value to the thread context
+        U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
+        if(!arch_reg_block_write_range(arch_info, reg_block, reg_rng, &reg_value))
+        {
+          unwind_status = MachineOpResult_Fail;
+          goto exit;
+        }
+      }break;
+      case DW_CFI_RegisterRule_Register:
+      {
+        // read register value from another register
+        U64 reg_size = dw_reg_size_from_code(arch, reg_idx);
+        if(!arch_reg_block_read_range(arch_info, reg_block, reg_rng, reg_buffer))
+        {
+          unwind_status = MachineOpResult_Fail;
+          goto exit;
+        }
+        
+        // write register value to the thread context
+        if(!arch_reg_block_write_range(arch_info, reg_block, reg_rng, reg_buffer))
+        {
+          unwind_status = MachineOpResult_Fail;
+          goto exit;
+        }
+        if (unwind_status != MachineOpResult_Ok) { goto exit; }
+      }break;
+      case DW_CFI_RegisterRule_Expression:
+      {
+        // TODO: evaluate expression
+        NotImplemented;
+      }break;
+      case DW_CFI_RegisterRule_ValExpression:
+      {
+        // TODO: evaluate expression
+        NotImplemented;
+      }break;
+      case DW_CFI_RegisterRule_Architectural:
+      {
+        NotImplemented;
+      }break;
     }
   }
-
+  
   // update stack pointer
-  U64 sp = dw_sp_from_arch(arch);
-  reg_write_func(sp, &cfa, sizeof(cfa), reg_write_ud);
-
-exit:;
+  arch_reg_block_write_sp(arch_info, reg_block, cfa);
+  
+  exit:;
   scratch_end(scratch);
   return unwind_status;
 }

commit 6fc10a4f4da0505367b00f762f60b16284e839cb

fix regression introduced in aea4a4ff

src/dwarf/dwarf_unwind.c
@@ -254,10 +254,10 @@ dw_compute_cfa(Arch arch, DW_CFI_Row *row, void *reg_block, MachineOp_MemRead *m
       Rng1U16 reg_rng = arch_info->reg_code_rng_table[reg_code];
       if(!arch_reg_block_read_range(arch_info, reg_block, reg_rng, &cfa_reg_value))
       {
-        unwind_status = MachineOpResult_Fail;
         break;
       }
       *cfa_out = cfa_reg_value + row->cfa.off;
+      unwind_status = MachineOpResult_Ok;
     }break;
     case DW_CFA_Rule_Expression:
     {
@@ -360,4 +360,3 @@ dw_cfi_apply_register_rules(Arch arch, U64 cfa, DW_CFI_Row *row, void *reg_block
   scratch_end(scratch);
   return unwind_status;
 }
-