a8f78cbc5d9cc0d32a3653ed77debc89fd574552
Nikita Smith <git.nikita.smith@gmail.com>
2026-07-20 14:17:31 -0700

    make const data eligible for ICF

--- src/linker/tests/linker_tests.c
+++ src/linker/tests/linker_tests.c
@@ -9249,7 +9249,7 @@ TEST(icf_readonly_non_vftable_data_policy)
 {
   U8 data_bytes[] = { 1, 2, 3, 4 };
   U8 entry_text[] = { 0xc3 };
-  U8 addresses[2 * sizeof(U64)] = {0};
+  U8 addresses[4 * sizeof(U64)] = {0};
 
   T_Ok(t_write_def_obj("icf_rdata_policy.obj", (T_COFF_DefObj){
     .machine = T_COFF_DefSetMachine(X64),
@@ -9257,12 +9257,15 @@ TEST(icf_readonly_non_vftable_data_policy)
       { "entry", ".text", str8_array_fixed(entry_text), .flags = "rx:code@1" },
       { "data_a", ".rdata$mn", str8_array_fixed(data_bytes), .flags = "r:data@1", .raw_flags = COFF_SectionFlag_LnkCOMDAT },
       { "data_b", ".rdata$mn", str8_array_fixed(data_bytes), .flags = "r:data@1", .raw_flags = COFF_SectionFlag_LnkCOMDAT },
-      { "addresses", ".data", str8_array_fixed(addresses), .flags = "rw:data@1", .relocs = (T_COFF_DefReloc[]){ T_COFF_DefReloc(X64_Addr64, 0, "data_a"), T_COFF_DefReloc(X64_Addr64, 8, "data_b"), {0} } },
+      { "data_c", ".rdata$mn", str8_array_fixed(data_bytes), .flags = "r:data@1", .raw_flags = COFF_SectionFlag_LnkCOMDAT },
+      { "data_d", ".rdata$mn", str8_array_fixed(data_bytes), .flags = "r:data@1", .raw_flags = COFF_SectionFlag_LnkCOMDAT },
+      { "addresses", ".data", str8_array_fixed(addresses), .flags = "rw:data@1", .relocs = (T_COFF_DefReloc[]){ T_COFF_DefReloc(X64_Addr64, 0, "data_a"), T_COFF_DefReloc(X64_Addr64, 8, "data_b"), T_COFF_DefReloc(X64_Addr64, 16, "data_c"), T_COFF_DefReloc(X64_Addr64, 24, "data_d"), {0} } },
       {0}
     },
     .symbols = (T_COFF_DefSymbol[]){
       T_COFF_DefSymbol_Secdef("data_a", COFF_ComdatSelect_NoDuplicates), T_COFF_DefSymbol_Secdef("data_b", COFF_ComdatSelect_NoDuplicates),
-      T_COFF_DefSymbol_ExternFunc("entry", "entry", 0), T_COFF_DefSymbol_Extern("data_a", "data_a", 0), T_COFF_DefSymbol_Extern("data_b", "data_b", 0), T_COFF_DefSymbol_Extern("addresses", "addresses", 0),
+      T_COFF_DefSymbol_Secdef("data_c", COFF_ComdatSelect_Any), T_COFF_DefSymbol_Secdef("data_d", COFF_ComdatSelect_Any),
+      T_COFF_DefSymbol_ExternFunc("entry", "entry", 0), T_COFF_DefSymbol_Extern("data_a", "data_a", 0), T_COFF_DefSymbol_Extern("data_b", "data_b", 0), T_COFF_DefSymbol_Extern("data_c", "data_c", 0), T_COFF_DefSymbol_Extern("data_d", "data_d", 0), T_COFF_DefSymbol_Extern("addresses", "addresses", 0),
       {0}
     }
   }));
@@ -9270,11 +9273,13 @@ TEST(icf_readonly_non_vftable_data_policy)
   t_invoke_linkerf("/subsystem:console /entry:entry /out:a.exe /opt:ref,icf /include:addresses icf_rdata_policy.obj");
   T_Ok(g_last_exit_code == 0);
 
-  U64 vaddrs[2] = {0};
+  U64 vaddrs[4] = {0};
   T_Ok(t_read_exe_data_vaddrs(arena, str8_lit("a.exe"), vaddrs, ArrayCount(vaddrs)));
   T_Ok(vaddrs[0] != 0);
   T_Ok(vaddrs[1] != 0);
   T_Ok(vaddrs[0] != vaddrs[1]);
+  T_Ok(vaddrs[2] != 0);
+  T_Ok(vaddrs[2] == vaddrs[3]);
 }
 
 TEST(icf_cpp_identical_functions_fold)
