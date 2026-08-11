7cc61e75e4061002764f85e0b3b91e721734a19f
Ryan Fleury <ryan.j.fleury@gmail.com>
2026-08-11 08:24:55 -0700

    fix use of old functions in spall profiling code

--- src/base/base_profile.c
+++ src/base/base_profile.c
@@ -8,22 +8,22 @@ spall_begin(char *fmt, ...)
   if(spall_buffer.data == 0)
   {
     spall_buffer.length = MB(1);
-    spall_buffer.data   = os_reserve(spall_buffer.length);
-    os_commit(spall_buffer.data, spall_buffer.length);
+    spall_buffer.data = reserve_memory(spall_buffer.length);
+    commit_memory(spall_buffer.data, spall_buffer.length);
     spall_buffer_init(&spall_profile, &spall_buffer);
   }
   if(spall_pid == 0)
   {
-    spall_pid = os_get_process_info()->pid;
+    spall_pid = get_process_info()->pid;
   }
   if(spall_tid == 0)
   {
-    spall_tid = os_tid();
+    spall_tid = tid();
   }
   Temp scratch = scratch_begin(0, 0);
   va_list args;
   va_start(args, fmt);
-  String8 string = push_str8fv(scratch.arena, fmt, args);
+  String8 string = str8fv(scratch.arena, fmt, args);
   spall_buffer_begin_ex(&spall_profile, &spall_buffer, string.str, string.size, now_time_us(), spall_tid, spall_pid);
   va_end(args);
   scratch_end(scratch);
